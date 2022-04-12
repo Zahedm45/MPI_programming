c62fdce204fcc955d1e9e8ab751250baa9ee742d
Zahed <zahedm45@gmail.com>
2022-04-12 13:31:09 +0200

    ...

--- week4/mpi_gather/gather.c
+++ week4/mpi_gather/gather.c
@@ -8,40 +8,24 @@
 
 
 int main (int argc, char *argv[]){
-
     int rank;
     int csize, localX;
-    //int arr[4];
     int *buf = NULL;
-
-
-
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &csize);
-
-
     localX = rank * 3;
     if (rank == 0) {
         buf =(int*) malloc(sizeof(int)*4);
     }
 
     MPI_Gather(&localX, 1, MPI_INT, buf, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-
-
-
-
     if (rank == 0) {
         for (int i = 0; i < 4; ++i) {
             printf(" %d", buf[i]);
         }
         printf("\n");
     }
-
-
-
-
     MPI_Finalize();
     return 0;
 }
